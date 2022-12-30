1b439e27b671df5526bfececbfb0a3dc22392150
ibeandyy <adhyr4@gmail.com>
2022-12-29 20:06:39 -0800

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import { providers, Contract } from "ethers";
 import { ActivityType, Client, IntentsBitField } from "discord.js";
 import { TOKEN, LP_ADDRESS, CHAINLINK_ADDRESS, PROVIDER } from "./config.json";
-import cnvabi from "./abis/cnvlp.json";
-import lpabi from "./abis/chainlink.json";
+import cnvABI from "./abis/cnvlp.json";
+import chainlinkABI from "./abis/chainlink.json";
 import wait from "wait";
 
 const client = new Client({
   intents: IntentsBitField.Flags.Guilds,
 });
 const provider = new providers.JsonRpcProvider(PROVIDER);
-const cnvContract = new Contract(LP_ADDRESS, lpabi, provider);
-const lpContract = new Contract(CHAINLINK_ADDRESS, cnvabi, provider);
+const cnvContract = new Contract(LP_ADDRESS, cnvABI, provider);
+const daiContract = new Contract(CHAINLINK_ADDRESS, chainlinkABI, provider);
 const main = async () => {
   await client.login(TOKEN);
   console.log("Logged in!");
   while (true) {
-    const daiPrice = (await lpContract.latestAnswer()) / 1e8;
-    const [cnvReserves, daiReserves] = await cnvContract.getReserves();
-    const priceInUsdInDollars = (
-      (daiPrice * daiReserves) /
-      cnvReserves
-    ).toFixed(2);
+    try {
+      const daiPrice = (await daiContract.latestAnswer()) / 1e8;
+      const [cnvReserves, daiReserves] = await cnvContract.getReserves();
+      const priceInUsdInDollars = (
+        (daiPrice * daiReserves) /
+        cnvReserves
+      ).toFixed(2);
 
-    client.user.setActivity(`CNV: $${priceInUsdInDollars}`, {
-      type: ActivityType.Watching,
-    });
-    await wait(10000);
+      client.user.setActivity(`CNV: $${priceInUsdInDollars}`, {
+        type: ActivityType.Watching,
+      });
+      await wait(100000);
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
 main();
